bc71e43556eb1b1b69929753d364314f8a08a99d
Will Toth <toth@revrobotics.com>
2018-11-28 01:00:58 -0600

    Adding everything except Read and Write since we are not using them for DFU modes

--- STTub30.go
+++ STTub30.go
@@ -18,6 +18,7 @@ import "C"
 
 import (
 	"fmt"
+	"strings"
 	"unsafe"
 
 	"github.com/willtoth/setupapi"
@@ -70,6 +71,8 @@ type DeviceDesc struct {
 
 type DeviceDesc C.USB_DEVICE_DESCRIPTOR
 type DeviceConfig C.USB_CONFIGURATION_DESCRIPTOR
+type InterfaceDesc C.USB_INTERFACE_DESCRIPTOR
+type EndpointDesc C.USB_ENDPOINT_DESCRIPTOR
 
 func checkSTError(code int) error {
 	if code != C.STDEVICE_NOERROR {
@@ -107,7 +110,7 @@ func (dev STDevice) GetStringDescriptor(nIndex uint) (string, error) {
 	nStringLength := uint(512)
 	var szStringBuf [512]byte
 	errno := C.STDevice_GetStringDescriptor(dev.handle, C.UINT(nIndex), C.LPSTR(unsafe.Pointer(&szStringBuf[0])), C.UINT(nStringLength))
-	return "", checkSTError(int(errno))
+	return strings.TrimRight(string(szStringBuf[:]), "\x00"), checkSTError(int(errno))
 }
 
 func (dev STDevice) GetDeviceDescriptor() (DeviceDesc, error) {
@@ -127,6 +130,161 @@ func (dev STDevice) GetConfigurationDescriptor(nConfigIdx uint) (DeviceConfig, e
 	var cfg C.USB_CONFIGURATION_DESCRIPTOR
 	errno := C.STDevice_GetConfigurationDescriptor(dev.handle, C.UINT(nConfigIdx), &cfg)
 	return DeviceConfig(cfg), checkSTError(int(errno))
+}
+
+func (dev STDevice) GetNbOfInterfaces(configIndex uint) (uint, error) {
+	var numInterfaces C.UINT
+	errno := C.STDevice_GetNbOfInterfaces(dev.handle, C.UINT(configIndex), &numInterfaces)
+	return uint(numInterfaces), checkSTError(int(errno))
+}
+
+func (dev STDevice) GetNbOfAlternateInterfaces(configIndex, interfaceIndex uint) (uint, error) {
+	var numAltInterfaces C.UINT
+	errno := C.STDevice_GetNbOfAlternates(dev.handle, C.UINT(configIndex), C.UINT(interfaceIndex), &numAltInterfaces)
+	return uint(numAltInterfaces), checkSTError(int(errno))
+}
+
+func (dev STDevice) GetInterfaceDescriptor(configIndex, interfaceIndex, nAltSetIdx uint) (InterfaceDesc, error) {
+	var desc C.USB_INTERFACE_DESCRIPTOR
+	errno := C.STDevice_GetInterfaceDescriptor(dev.handle, C.UINT(configIndex), C.UINT(interfaceIndex), C.UINT(nAltSetIdx), &desc)
+	return InterfaceDesc(desc), checkSTError(int(errno))
+}
+
+func (dev STDevice) GetNbOfEndPoints(configIndex, interfaceIndex, nAltSetIdx uint) (uint, error) {
+	var numEndpoints C.UINT
+	errno := C.STDevice_GetNbOfEndPoints(dev.handle,
+		C.UINT(configIndex),
+		C.UINT(interfaceIndex),
+		C.UINT(nAltSetIdx),
+		&numEndpoints)
+	return uint(numEndpoints), checkSTError(int(errno))
+}
+
+func (dev STDevice) GetEndPointDescriptor(configIndex, interfaceIndex, nAltSetIdx, nEndPointIdx uint) (EndpointDesc, error) {
+	var desc C.USB_ENDPOINT_DESCRIPTOR
+	errno := C.STDevice_GetEndPointDescriptor(dev.handle,
+		C.UINT(configIndex),
+		C.UINT(interfaceIndex),
+		C.UINT(nAltSetIdx),
+		C.UINT(nEndPointIdx),
+		&desc)
+	return EndpointDesc(desc), checkSTError(int(errno))
+}
+
+func (dev STDevice) GetNbOfDescriptors(nLevel, nType byte, configIndex, interfaceIndex, nAltSetIdx, nEndPointIdx uint) (uint, error) {
+	var numDescriptors C.UINT
+	errno := C.STDevice_GetNbOfDescriptors(dev.handle,
+		C.BYTE(nLevel),
+		C.BYTE(nType),
+		C.UINT(configIndex),
+		C.UINT(interfaceIndex),
+		C.UINT(nAltSetIdx),
+		C.UINT(nEndPointIdx),
+		&numDescriptors)
+	return uint(numDescriptors), checkSTError(int(errno))
+}
+
+func (dev STDevice) GetDescriptor(nLevel, nType byte, configIndex, interfaceIndex, nAltSetIdx, nEndPointIdx, nIdx uint) (string, error) {
+	szDesc := uint(512)
+	var pDesc [512]byte
+	errno := C.STDevice_GetDescriptor(dev.handle,
+		C.BYTE(nLevel),
+		C.BYTE(nType),
+		C.UINT(configIndex),
+		C.UINT(interfaceIndex),
+		C.UINT(nAltSetIdx),
+		C.UINT(nEndPointIdx),
+		C.UINT(nIdx),
+		C.PBYTE(unsafe.Pointer(&pDesc[0])),
+		C.UINT(szDesc))
+	return strings.TrimRight(string(pDesc[:]), "\x00"), checkSTError(int(errno))
+}
+
+func (dev STDevice) SelectCurrentConfiguration(configIndex, interfaceIndex, nAltSetIdx uint) error {
+	errno := C.STDevice_SelectCurrentConfiguration(dev.handle,
+		C.UINT(configIndex),
+		C.UINT(interfaceIndex),
+		C.UINT(nAltSetIdx))
+	return checkSTError(int(errno))
+}
+
+func (dev STDevice) SetDefaultTimeout(timeout int) error {
+	errno := C.STDevice_SetDefaultTimeOut(dev.handle, C.DWORD(timeout))
+	return checkSTError(int(errno))
+}
+
+func (dev STDevice) SetSuspendModeBehaviour(allow bool) error {
+	var doAllow C.BOOL
+	if allow {
+		doAllow = 1
+	} else {
+		doAllow = 0
+	}
+	errno := C.STDevice_SetSuspendModeBehaviour(dev.handle, doAllow)
+	return checkSTError(int(errno))
+}
+
+const (
+	endpointPipeReset     = C.PIPE_RESET
+	endpointAbortTransfer = C.ABORT_TRANSFER
+)
+
+func (dev STDevice) EndPointControl(nEndPointIdx, nOperation uint) error {
+	errno := C.STDevice_EndPointControl(dev.handle,
+		C.UINT(nEndPointIdx),
+		C.UINT(nOperation))
+	return checkSTError(int(errno))
+}
+
+func (dev STDevice) Reset() error {
+	errno := C.STDevice_Reset(dev.handle)
+	return checkSTError(int(errno))
+}
+
+const (
+	descriptorConfigLevel         = C.DESCRIPTOR_CONFIGURATION_LEVEL
+	descriptorIntfAltSettingLevel = C.DESCRIPTOR_INTERFACEALTSET_LEVEL
+	descriptorEndpointLevel       = C.DESCRIPTOR_ENDPOINT_LEVEL
+
+	urbVendorDevice    = C.URB_FUNCTION_VENDOR_DEVICE
+	urbVendorInterface = C.URB_FUNCTION_VENDOR_INTERFACE
+	urbVendorEndpoint  = C.URB_FUNCTION_VENDOR_ENDPOINT
+	urbVendorOther     = C.URB_FUNCTION_VENDOR_OTHER
+
+	urbClassDevice    = C.URB_FUNCTION_CLASS_DEVICE
+	urbClassInterface = C.URB_FUNCTION_CLASS_INTERFACE
+	urbClassEndpoint  = C.URB_FUNCTION_CLASS_ENDPOINT
+	urbClassOther     = C.URB_FUNCTION_CLASS_OTHER
+
+	vendorDirectionIn  = C.VENDOR_DIRECTION_IN
+	vendorDirectionOut = C.VENDOR_DIRECTION_OUT
+)
+
+type ControlPipeRequest struct {
+	Function  uint16
+	Direction uint64
+	Request   byte
+	Value     uint16
+	Index     uint16
+	Length    uint64
+}
+
+func (dev STDevice) ControlPipeRequest(Request ControlPipeRequest, Data []byte) error {
+	var req C.CNTRPIPE_RQ
+
+	req.Function = C.USHORT(Request.Function)
+	req.Direction = C.ULONG(Request.Direction)
+	req.Request = C.UCHAR(Request.Request)
+	req.Value = C.USHORT(Request.Value)
+	req.Index = C.USHORT(Request.Index)
+	req.Length = C.ULONG(Request.Length)
+
+	//var buffer [512]byte
+
+	//copy(buffer[:], Data)
+
+	errno := C.STDevice_ControlPipeRequest(dev.handle, &req, C.PBYTE(unsafe.Pointer(&Data[0])))
+	return checkSTError(int(errno))
 }
 
 func main() {
@@ -158,7 +316,14 @@ func main() {
 		return
 	}
 
-	fmt.Printf("%s\r\n", desc)
+	manStr, err := dev.GetStringDescriptor(uint(desc.iManufacturer))
+
+	if err != nil {
+		fmt.Printf("Failed to get device string descriptor: %v", err)
+		return
+	}
+
+	fmt.Printf("%s\n", manStr)
 
 	err = dev.Close()
 
